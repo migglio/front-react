688a7dca673173eeb7afe8dad1cf33573ff8a6b1
jrdahl <jdahl@mg-group.com.ar>
2019-10-17 20:02:23 -0300

    improvemnets home

--- src/components/Home/RegisterNewTrip.js
+++ src/components/Home/RegisterNewTrip.js
@@ -1,15 +1,31 @@
 import React from "react";
 import { withStyles } from "material-ui/styles";
-import { Grid } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 const styles = theme => ({
+  root: {
+    display: "flex",
+    padding: "4%",
+    justifyContent: "center",
+    textAlign: "center"
+  },
+  imageContainer: {
+    display: "none",
+    "@media (min-width:768px)": {
+      display: "flex"
+    }
+  },
   paper2: {
-    padding: theme.spacing.unit,
-    textAlign: "left",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    maxWidth: "90%",
     color: theme.palette.text.secondary
   },
   button: {
+    marginTop: "20px",
     textAlign: "center",
     margin: theme.spacing.unit
   },
@@ -27,33 +43,29 @@ class RegisterNewTrip extends React.Component {
     const { classes } = this.props;
 
     return (
-      <div>
-        <Grid container spacing={16} className={classes.demo}>
-          <img
-            alt=""
-            src={require("../../driver.jpg")}
-            width="30%"
-            height="30%"
-          />
-          <Grid item className={classes.paper2}>
-            <h1> ¿Cansado de viajar solo?</h1>
-            <h1>Encuentra una compañia de viaje..</h1>
-            <p>
-              {" "}
-              Conoce nueva gente, convierte tu viaje mas divertido y económico.{" "}
-            </p>
-            <Grid item className={classes.button}>
-              <Button
-                href="../TripCreator"
-                className={classes.button}
-                variant="raised"
-                color="primary"
-              >
-                Nuevo viaje
-              </Button>
-            </Grid>
-          </Grid>
-        </Grid>
+      <div className={classes.root}>
+        <div className={classes.imageContainer}>
+          <img alt="" src={require("../../driver.jpg")} />
+        </div>
+        <div className={classes.paper2}>
+          <Typography variant="display1">¿Cansado de viajar solo?</Typography>
+          <Typography variant="display1">
+            Encuentra una compañia de viaje..
+          </Typography>
+          <div style={{ paddingTop: 20, paddingBottom: 20, maxWidth: "300px" }}>
+            <Typography variant="headline">
+              Conoce nueva gente, convierte tu viaje más divertido y económico.
+            </Typography>
+          </div>
+          <Button
+            href="../TripCreator"
+            className={classes.button}
+            variant="raised"
+            color="primary"
+          >
+            Nuevo viaje
+          </Button>
+        </div>
       </div>
     );
   }
